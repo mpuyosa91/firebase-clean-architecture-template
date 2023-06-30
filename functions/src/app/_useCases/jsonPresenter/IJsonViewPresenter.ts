8b120a2e26a7dbcbf4244271e4a4f5fbe8501448
Moises E. Puyosa <mpuyosa91@gmail.com>
2023-06-30 15:41:52 -0500

    Create JsonPresenter

---
+++ functions/src/app/_useCases/jsonPresenter/IJsonViewPresenter.ts
@@ -0,0 +1,31 @@
+export interface IJsonViewPresenter {
+
+  /**
+   *
+   * @param request
+   * @param response
+   * @param {(data: object, context: object) => Promise<any>} callback
+   * @returns {Promise<any>}
+   */
+  getOnRequestAsOnCall(
+    request: any,
+    response: any,
+    callback: (data: object, context: object) => Promise<any>,
+  ): Promise<any>;
+
+  /**
+   *
+   * @param {string} functionName
+   * @param {object} data
+   * @param {object} context
+   * @param {(data: object, user: string) => Promise<object>} callback
+   * @returns {Promise<object>}
+   */
+  executeIfAuthorizedAndManageErrorsIf(
+    functionName: string,
+    data: object,
+    context: object,
+    callback: (data: object, user: string) => Promise<object>,
+  ): Promise<object>;
+
+}
---
+++ functions/src/app/_useCases/jsonPresenter/index.ts
@@ -0,0 +1,2 @@
+export * from './IJsonViewPresenter';
+export * from './IJsonViewPresenterDriver';
