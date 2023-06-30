9295731cf5360c08c997fbc81b79986f751c7b50
Moises E. Puyosa <mpuyosa91@gmail.com>
2023-06-30 15:03:01 -0500

    Update .eslintrc.js

---
+++ functions/.eslintrc.js
@@ -0,0 +1,22 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-console': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        varsIgnorePattern: '^_$',
+        argsIgnorePattern: '^_$',
+        caughtErrorsIgnorePattern: '^_$',
+      },
+    ],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+};
